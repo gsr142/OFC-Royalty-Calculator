ad6a2b71397808d73f6a24418b45db493426d287
gsr142 <76536836+gsr142@users.noreply.github.com>
2023-02-09 09:48:42 -0800

    Added header for hand charts changed classes.py to classes_file.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import classes
+import classes_file
 import pandas as pd
 st.title("OFC Royalty Calculator")
 # Load the data frames
@@ -11,14 +11,15 @@ number_of_players = st.selectbox("Select Number of Players", ("2", "3", "4"))
 
 # Set page up for number of players
 if number_of_players == "4":
-    classes.Calculate.four_players(number_of_players)
+    classes_file.Calculate.four_players(number_of_players)
 
 elif number_of_players == "3":
-    classes.Calculate.three_players(number_of_players)
+    classes_file.Calculate.three_players(number_of_players)
 
 else:
-    classes.Calculate.two_players(number_of_players)
+    classes_file.Calculate.two_players(number_of_players)
 
+st.header("Quick Reference Hand Charts")
 col1, col2, col3 = st.columns(3)
 
 with col1:
